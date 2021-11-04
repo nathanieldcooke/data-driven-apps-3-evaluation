da784507bc037ee329980563b65b2aa7935ade52
nathanieldcooke <nathanield.cooke@gmail.com>
2021-11-04 09:02:29 -0700

    create book model and migration

--- config/index.js
+++ config/index.js
@@ -1,6 +1,10 @@
-console.log('THIS PORT: ', process.env.NODE_ENV)
-
 module.exports = {
     environment: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 8080,
+    db: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+    }
 };
